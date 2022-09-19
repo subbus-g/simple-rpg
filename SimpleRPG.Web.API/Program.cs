8e6eee6dee4d93d1dfb22b00b8b3af3a863a74ee
subbus <subbareddyg83@gmail.com>
2022-09-19 12:03:36 +0530

    Update Swagger settings

--- SimpleRPG.Web.API/Program.cs
+++ SimpleRPG.Web.API/Program.cs
@@ -14,12 +14,12 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+   
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
